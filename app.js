47e18f1aeaa17d8ae2402093f49f9ba486613493
xuefeng <xuefeng@data-god.com>
2018-08-20 15:21:02 +0800

    2018-08-20 15:21:02

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
 const superagent = require('superagent'),
 cheerio = require('cheerio'),
 config = require('./config'),
-schedule = require('node-schedule'),
+schedule = require('node-schedule'),      //定时器
 now = config.dateFomat(new Date()),
 getNetUrl = "http://caipiao.163.com/award/cqssc/",
 getMyUrl = `http://120.26.211.107/period/20180806${now}`,
 addNumUrl = `http://120.26.211.107/period/20180806${now}`;
 let netData = [],
-myData = [],
-c=2;
+myData = [];
 
+console.log(now);
 //获取网易彩票信息
-function getAward(){
-  superagent.get(getNetUrl).end(function (err,res) {
+async function getAward(){
+  let res = await  superagent.get(getNetUrl),
+    $ = cheerio.load(res.text);
     /**
    * res.text 包含未解析前的响应内容
    * 我们通过cheerio的load方法解析整个文档，就是html页面所有内容，可以通过console.log($.html());在控制台查看
    */
-    let $ = cheerio.load(res.text);
     $(".award-winNum").each(function(i,item){
     netData[i] = $(item).html();
     });
-    console.log(netData);
+}
 
-    //获取原来数据
-    superagent.get(getMyUrl).end(function (err,res) {
-      JSON.parse(res.text).periods.forEach(function(item,i){
-        myData[i] = item.numbers;
-      });
-      console.log(myData);
-      checkNum(netData,myData);
-    });
+async function checkAward(){
+  let res = await superagent.get(getMyUrl);
+  JSON.parse(res.text).periods.forEach(function(item,i){
+    myData[i] = item.numbers;
   });
+  checkNum(netData,myData);
 }
 
 //检测，如果发现有不同的中奖号码，更新
